63b02ee07141716a35cff1c03b8d2d10f8aaa0db
srmartins19 <86935614+srmartins19@users.noreply.github.com>
2026-03-18 16:58:16 -0300

    Delete app directory

--- app/rfqs/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Sidebar } from '@/components/layout/sidebar';
-export default function RfqsLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex min-h-screen bg-slate-50">
-      <Sidebar />
-      <div className="flex-1 ml-60 flex flex-col">{children}</div>
-    </div>
-  );
-}
